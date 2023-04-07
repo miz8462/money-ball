e2cbea96ea1ffe36718b2ed518e81444dc9344f3
mizki <wwr8462@gmail.com>
2023-04-07 14:48:44 +0900

    選手とチームのモデルを作成

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.team_name
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=50)
+    born = models.DateField(default="2000-01-01")
+    height = models.IntegerField(default=180)
+    weight = models.IntegerField(default=80)
+    salary = models.IntegerField(default=1000) # 万円
+    uniform_number = models.IntegerField(default=0)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
